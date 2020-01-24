9107feef359ad0d2c2d5a7570c01c005cd5de5f0
Eduard Abrakov <abrakovem@mail.ru>
2020-01-24 15:50:56 +0500

    create parser

---
+++ src/test/java/main/CsvParserTest.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CsvParserTest extends Assert {
+
+    @Test
+    public void parse() {
+    }
+}
---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest extends Assert {
+
+    @Test
+    public void main() {
+    }
+}
